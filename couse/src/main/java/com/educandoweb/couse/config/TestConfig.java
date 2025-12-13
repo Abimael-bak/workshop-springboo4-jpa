05a06f134e2bf3d61ddc15f01b843231aed9da1c
Abimael-bak <abimaelabreu88@gmail.com>
2025-12-13 10:18:15 -0300

    Product

--- couse/src/main/java/com/educandoweb/couse/config/TestConfig.java
+++ couse/src/main/java/com/educandoweb/couse/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.couse.entities.Category;
 import com.educandoweb.couse.entities.Order;
+import com.educandoweb.couse.entities.Product;
 import com.educandoweb.couse.entities.User;
 import com.educandoweb.couse.entities.enums.OrderStatus;
 import com.educandoweb.couse.repositories.CategoryRepository;
 import com.educandoweb.couse.repositories.OrderRepository;
+import com.educandoweb.couse.repositories.ProductRepository;
 import com.educandoweb.couse.repositories.UserRepository;
 
 @Configuration
@@ -30,6 +32,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -45,10 +50,16 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null,"Books");
 		Category cat3 = new Category(null,"Commputers");
 		
-		
+		Product p1 = new Product(null,"The lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null,"Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
+		Product p3 = new Product(null,"Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null,"PC Games", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null,"Rails for Dummies", "Cras fringilla convallias sem vel faucibus.", 100.99, "");
+		 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 	}
 	
 } 
