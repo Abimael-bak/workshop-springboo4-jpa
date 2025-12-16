3ca08c9772deebe8f55f733882d8ddd88a39e243
Abimael-bak <abimaelabreu88@gmail.com>
2025-12-16 12:15:55 -0300

    OrderItem, many-to-many association with extra attributes PART 2

---
+++ couse/src/main/java/com/educandoweb/couse/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.couse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.couse.entities.OrderItem;
+import com.educandoweb.couse.entities.pk.*;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+
+
+}
